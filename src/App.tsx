1b9dfcf6913a748377a7bd83d266fe5d1b8506dd
Febri-i <febribayun@gmail.comm>
2023-10-01 08:58:11 +0700

    finishing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     const handleBacaSurat: (infoSurat: InfoSurat) => void = (infoSurat: InfoSurat) => {
         setSuratDibaca(infoSurat);
 
-        fetch("/surat/" + infoSurat.nomor)
+        fetch("./surat/" + infoSurat.nomor)
             .then(res => res.text())
             .then(data => {
                 setAyatDibaca(JSON.parse(data));
@@ -43,8 +43,8 @@ function App() {
         <div className="w-full overflow-x-hidden h-full text-gray-700">
             <AnimatePresence>
 
-                {(suratDibaca.nomor < 0) && <motion.div key={"daftarSurat"} className=" w-96 absolute top-0 left-1/2 -translate-x-1/2" transition={{ type: "tween", ease: "easeOut" }} initial={variant.initial} exit={variant.exit} animate={variant.animate}><DaftarSurat bacaSuratCb={handleBacaSurat} /></motion.div>}
-                {(suratDibaca.nomor > 0) && <motion.div key={"pembacaSurat"} transition={{ type: "tween", ease: "easeOut" }} initial={variant.initial} animate={variant.animate} exit={variant.exit} className="w-96 absolute left-1/2  ">
+                {(suratDibaca.nomor < 0) && <motion.div key={"daftarSurat"} className=" w-96 sm:80 absolute top-0 left-1/2 -translate-x-1/2" transition={{ type: "tween", ease: "easeOut" }} initial={variant.initial} exit={variant.exit} animate={variant.animate}><DaftarSurat bacaSuratCb={handleBacaSurat} /></motion.div>}
+                {(suratDibaca.nomor > 0) && <motion.div key={"pembacaSurat"} transition={{ type: "tween", ease: "easeOut" }} initial={variant.initial} animate={variant.animate} exit={variant.exit} className="w-96 sm:w-80 absolute left-1/2  ">
                     <PembacaSurat daftarAyat={ayatDibaca} infoSurat={suratDibaca} kembaliCb={handleKembali} />
                 </motion.div>}
 
